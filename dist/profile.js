f2cc45bbc8f4c90d8b159bd9a6da26340f69b3f2
Viktoria <doduchenko29@gmail.com>
2021-07-03 12:19:30 +0300

    add

---
+++ dist/profile.js
@@ -0,0 +1,8 @@
+// file path: src/profile.js
+const printProfile = function (profileData) {
+  const {
+    name,
+    company
+  } = profileData;
+  console.log("".concat(name, " from ").concat(company));
+};
